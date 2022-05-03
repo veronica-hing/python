459cceab4999eb579ba6b55ea2ae260341c63e58
veronica-hing <veronica.m.hing@gmail.com>
2022-05-02 21:40:32 -0700

    added chaining capabilites by having methods return self

--- fundamentals/oop/user.py
+++ fundamentals/oop/user.py
@@ -7,18 +7,22 @@ class User:
     def make_deposit(self, amount):
         #increases user's balance by amount specified
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         #decrease user's balance by amount specified
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         #prints user name and account balance to terminal
         print(f"{self.name} {self.account_balance}")
+        return self
         
     def transfer_money(self, other_user, amount):
         self.make_withdrawal(amount)
         other_user.make_deposit(amount)
+        return self
 
 betty = User("Betty Boop", "betty@boop.com")
 annie = User("Annie Ok", "annie_r_u_ok@helpmail.com")
@@ -26,27 +30,28 @@ stacy = User("Stacy Mom", "gotItGoing@onmail.com")
 
 ##make first user do 3 deposits and 1 withdrawal
 betty.display_user_balance()
-betty.make_deposit(300)
-betty.make_deposit(30)
-betty.make_deposit(9)
-betty.make_withdrawal(89)
-betty.display_user_balance()
+betty.make_deposit(300).make_deposit(30).make_deposit(9).make_withdrawal(89).display_user_balance()
+#betty.make_deposit(30)
+#betty.make_deposit(9)
+#betty.make_withdrawal(89)
+#betty.display_user_balance()
 
 ##make second user do 2 deposits and 2 withdrawals
-annie.make_deposit(2222)
-annie.make_deposit(77)
-annie.display_user_balance()
+annie.make_deposit(2222).make_deposit(77).make_withdrawal(99).make_withdrawal(32).display_user_balance()
+#annie.make_deposit(77)
+#annie.make_withdrawal(32)
+#annie.make_withdrawal(99)
+#annie.display_user_balance()
 
 ##make third user do 1 deposit and 3 withdrawals
 
-stacy.make_deposit(9999)
-stacy.make_withdrawal(66)
-stacy.make_withdrawal(300)
-stacy.make_withdrawal(33)
-stacy.display_user_balance()
+stacy.make_deposit(9999).make_withdrawal(66).make_withdrawal(300).make_withdrawal(33).display_user_balance()
+#stacy.make_withdrawal(66)
+#stacy.make_withdrawal(300)
+#stacy.make_withdrawal(33)
+#stacy.display_user_balance()
 
 ## stacy transfers money to annie
 
-stacy.transfer_money(annie, 600)
-stacy.display_user_balance()
+stacy.transfer_money(annie, 600).display_user_balance()
 annie.display_user_balance()
